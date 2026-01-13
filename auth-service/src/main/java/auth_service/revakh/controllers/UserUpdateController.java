bef1495d5006aa26b2d7b8f5f048bb5ee2c2a2aa
JeethanTauro <tauroshanjeeth@gmail.com>
2026-01-13 14:00:15 +0530

    created a userUpdateController for updating user profile, haven't written code yet

--- auth-service/src/main/java/auth_service/revakh/controllers/UserUpdateController.java
+++ auth-service/src/main/java/auth_service/revakh/controllers/UserUpdateController.java
@@ -1,4 +1,9 @@
 package auth_service.revakh.controllers;
 
 public class UserUpdateController {
+    //update username
+    //update phone number
+
+    //also just to remember that see when a the user gets an income, is it a reimbersement for a particular category? or just a top up or a salary
+
 }
